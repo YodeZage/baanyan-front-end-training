9832117fb27fd49c4afc1dc4e06dad8c716761da
Yode Desktop <yaozhenkang@gmail.com>
2020-07-04 00:46:29 -0400

    Add template form examples

--- angular-practice/angular-playground/src/app/forms/components/template-form/template-form.component.ts
+++ angular-practice/angular-playground/src/app/forms/components/template-form/template-form.component.ts
@@ -1,23 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-template-form',
   templateUrl: './template-form.component.html',
   styleUrls: ['./template-form.component.css']
 })
-export class TemplateFormComponent implements OnInit {
+export class TemplateFormComponent {
+
+  @ViewChild('heroForm') heroForm: ElementRef;
+
+  readonly superPowerList: string[] = [
+    'Super fast', 'Super flexible',
+    'Super rich', 'Super hot'
+  ];
 
   model = {
-    name: 'Hello, World!'
+    name: 'Bruce Wayne',
+    age: 26,
+    superName: this.superPowerList[2]
   };
+
+  nameError: string;
 
   constructor() { }
 
-  ngOnInit(): void {
+  onSubmit() {
+    if (this.checkNameField()) {
+      console.log(this.model);
+      console.log(this.heroForm);
+    } else {
+      console.log('Form is NOT submitted!');
+    }
   }
 
-  onSubmit() {
-    console.log(this.model);
+  checkNameField(): boolean {
+    const namePattern = /^[a-z\sA-Z]*$/;
+    const minLength = /.{2,}/;
+
+    if (!namePattern.test(this.model.name)) {
+      this.nameError = 'Full name must contains letters and space only.';
+      return false;
+    }
+
+    if (!minLength.test(this.model.name)) {
+      this.nameError = 'Full name should more than two letters.';
+      return false;
+    }
+
+    this.nameError = undefined;
+    return true;
   }
 
 }
