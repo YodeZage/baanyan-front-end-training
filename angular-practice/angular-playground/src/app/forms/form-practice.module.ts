ba8e1c6f035f6c4df2c77fddfda1d90d4b905c0a
Yode Desktop <yaozhenkang@gmail.com>
2020-07-24 12:21:01 -0400

    Add some changes

--- angular-practice/angular-playground/src/app/forms/form-practice.module.ts
+++ angular-practice/angular-playground/src/app/forms/form-practice.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormPracticeRoutingModule } from './forms-practice-routing.module';
 import { CoreModule } from '../core/core.module';
 
